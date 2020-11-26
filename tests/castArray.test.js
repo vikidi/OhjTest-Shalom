73ad00dc207878ee12abf3edd7fd7c5b99c47b09
Ville Saarinen <ville.saarinen9@gmail.com>
2020-11-26 15:55:38 +0200

    castArray tests

--- tests/castArray.test.js
+++ tests/castArray.test.js
@@ -5,12 +5,47 @@ import castArray from '../src/castArray';
  * 
  */
 
+/* Found Errors
+ *
+ * Casts array to another array resulting in "double array". Should not. Label: Error
+ * 
+ * Casting empty parameter list adds "undefined" to the array. Label: Error. 
+ */
+
 describe('castArray()', () => {
   describe('positive tests', () => {
-    test.todo('')
+    test('Cast number', () => {
+      const num = 1;
+      expect(castArray(num)).toStrictEqual([num])
+    })
+
+    test('Cast string', () => {
+      const string = 'abc'
+      expect(castArray(string)).toStrictEqual([string])
+    })
+
+    test('Cast array', () => {
+      const arr = [1, 'abc']
+      expect(castArray(arr)).toStrictEqual([arr])
+    })
+
+    test('Cast object', () => {
+      const obj = {a: 1, b: 'abc'}
+      expect(castArray(obj)).toStrictEqual([obj])
+    })
   })
 
   describe('negative tests', () => {
-    test.todo('')
+    test('Cast empty param list', () => {
+      expect(castArray()).toStrictEqual([])
+    })
+
+    test('Cast null', () => {
+      expect(castArray(null)).toStrictEqual([null])
+    })
+
+    test('Cast undefined', () => {
+      expect(castArray(undefined)).toStrictEqual([undefined])
+    })
   })
 })
